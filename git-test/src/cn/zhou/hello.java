10231936a763c9863971ec7fc8ae4a0f636df667
zhou <94497950@qq.com>
2020-07-18 16:34:02 +0800

    修改内容

---
+++ git-test/src/cn/zhou/hello.java
@@ -0,0 +1,10 @@
+package cn.zhou;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+
+
+        System.out.println("添加测试文件");
+    }
+}
